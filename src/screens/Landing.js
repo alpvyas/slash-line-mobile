03e1b89c9865b917e642931fe1b8dc46cbc65298
Alpesh Vyas <alpesh@Alpeshs-iMac.attlocal.net>
2021-08-30 17:45:52 -0700

    basic set up and configuration

---
+++ src/screens/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Landing = () => {
+  const message = 'Slash Line Baseball';
+
+  return (
+    <>
+      <Text>{message}</Text>
+    </>
+  );
+};
+
+export default Landing;
